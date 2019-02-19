8a3c681a8bce5da26518b7cc69c0c9b6a134fa28
Andrew Baxter (student) <a.baxter.1@research.gla.ac.uk>
2019-02-19 13:38:19 +0000

    Reformatted to fit 80-character limit

--- Read HFD total births.R
+++ Read HFD total births.R
@@ -8,30 +8,78 @@ library(gganimate)
 
 allbirths <- read_tsv("birthsRR.txt")
 ccodes <- read_tsv("country_codes.txt")
-allbirths[["Country"]]<- pull(ccodes[match(allbirths[["Code"]], ccodes[["Code"]]), "Country"])
-country_names<-c("Austria", "Australia", "Belgium", "Bulgaria", "Canada", "Croatia", "Cyprus",
-                 "Czechia", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", 
-                 "Hungary", "Iceland", "Ireland", "Italy", "Latvia", "Lithuania", "Luxembourg",
-                 "Malta","Netherlands", "New Zealand", "Norway", "Poland", "Portugal", "Romania",
-                 "Slovak Republic", "Slovenia", "Spain", "Sweden", "Switzerland", "United Kingdom",
-                 "Scotland", "England and Wales", "Northern Ireland",
-                 "United States of America")
+allbirths[["Country"]] <-
+  pull(ccodes[match(allbirths[["Code"]], ccodes[["Code"]]), "Country"])
+country_names <-
+  c(
+    "Austria",
+    "Australia",
+    "Belgium",
+    "Bulgaria",
+    "Canada",
+    "Croatia",
+    "Cyprus",
+    "Czechia",
+    "Denmark",
+    "Estonia",
+    "Finland",
+    "France",
+    "Germany",
+    "Greece",
+    "Hungary",
+    "Iceland",
+    "Ireland",
+    "Italy",
+    "Latvia",
+    "Lithuania",
+    "Luxembourg",
+    "Malta",
+    "Netherlands",
+    "New Zealand",
+    "Norway",
+    "Poland",
+    "Portugal",
+    "Romania",
+    "Slovak Republic",
+    "Slovenia",
+    "Spain",
+    "Sweden",
+    "Switzerland",
+    "United Kingdom",
+    "Scotland",
+    "England and Wales",
+    "Northern Ireland",
+    "United States of America"
+  )
 
 allbirths %>%
   mutate(Age=as.numeric(sub("-", "", Age))) %>%
   filter(Age>12 & Age<21) -> teenbirths
 
-country_names %>% subset(!(country_names %in% allbirths$Country)) # countries with missing data
-allbirths$Country %>% subset(!(allbirths$Country %in% country_names)) %>% unique() # countries included in database, not used here
+# countries with missing data
+country_names %>%
+  subset(!(country_names %in% allbirths$Country))
 
-allbirths %>% filter(is.na(Country)) %>% group_by(Code) %>% summarise(n=n()) # find codes not translated
+# countries included in database, not used here
+allbirths$Country %>%
+  subset(!(allbirths$Country %in% country_names)) %>%
+  unique() 
+
+# find codes not translated
+allbirths %>% 
+  filter(is.na(Country)) %>%
+  group_by(Code) %>%
+  summarise(n=n()) 
 
 # Read population file -----------------------------------------------------
 
 readpop <- function(code) {
-  filename <- paste("T:/projects/Student-ABaxter/DataAnalysis/Population/", code, ".Population.txt",sep="")
-  pop <- read.fwf(filename, widths = c(7,13,20,16,16),sep="", skip=2, header=TRUE)
-  pop <- dplyr::mutate(pop, Code=code, Age=as.numeric(as.character(Age)), Year=as.numeric(as.character(Year)))
+  filename <- paste("T:/projects/Student-ABaxter/DataAnalysis/Population/",
+                    code, ".Population.txt",sep="")
+  pop <- read.fwf(filename, widths = c(7,13,20,16,16),sep="", skip=2,
+                  header=TRUE)
+  pop <- dplyr::mutate(pop, Code=code, Age=as.numeric(as.character(Age)),
+                       Year=as.numeric(as.character(Year)))
 #  left_join(teenbirths, pop[c(1,2,3,6)], by=c("Year","Age","Code"))
 #  assign(paste(code,"_pop", sep=""), pop, .GlobalEnv)
 }
@@ -67,17 +115,23 @@ allpops <- allpopsroot %>%
 
 # Combine with births -----------------------------------------------------
 
-teenrates <- left_join(teenbirths, allpops[c(1,2,5,6)], by=c("Year","Age","Code")) %>% rename(Femalepop = Female)
+teenrates <- left_join(teenbirths,
+                       allpops[c(1,2,5,6)],
+                       by=c("Year","Age","Code")) %>%
+  rename(Femalepop = Female)
 
 
 
 
 
 # Group and calculate -----------------------------------------------------
-# have included age 12 births here - check ONS guidlines and methods and report appropriately
+# have included age 12 births here
+# - check ONS guidlines and methods and report appropriately
 
-agegrps<-tibble(Age=factor(c(12:15, 12:17, 12:19)), agegrp=c(1,1,1,1,2,2,2,2,2,2,3,3,3,3,3,3,3,3)) # denominators for rate calc
-agecats<-tibble(Age=factor(c(13:15, 15:17, 15:19)), agecat=c(1,1,1,2,2,2,3,3,3,3,3)) # numerators for rate calc
+agegrps<-tibble(Age=factor(c(12:15, 12:17, 12:19)),
+                agegrp=c(1,1,1,1,2,2,2,2,2,2,3,3,3,3,3,3,3,3)) # denominators for rate calc
+agecats<-tibble(Age=factor(c(13:15, 15:17, 15:19)),
+                agecat=c(1,1,1,2,2,2,3,3,3,3,3)) # numerators for rate calc
 agecalcs<-merge(agegrps, agecats)
 
 birthrates <- teenrates %>%
@@ -87,7 +141,8 @@ birthrates <- teenrates %>%
   summarise(popsum=sum(Femalepop), birthsum=sum(Total)) %>%
   ungroup() %>%
   filter(agegrp==agecat) %>%
-  mutate(agecat=factor(agecat, labels = c("Under 16", "Under 18", "Under 20"))) %>%
+  mutate(agecat=factor(agecat,
+                       labels = c("Under 16", "Under 18", "Under 20"))) %>%
   mutate(rate=birthsum/popsum)
   
 # Graphs of all -----------------------------------------------------
@@ -101,7 +156,8 @@ birthrates %>%
     theme_minimal() +
     facet_wrap(~agecat) +
 #  geom_smooth(aes(col=NULL,group=NULL), size=1.5, col="steelblue")
-    stat_summary(aes(col=NULL, group=NULL), fun.y=mean, geom="line", size=1.5, col="steelblue") 
+    stat_summary(aes(col=NULL, group=NULL),
+                 fun.y=mean, geom="line", size=1.5, col="steelblue") 
 #    stat_summary(aes(col=NULL,group=NULL), geom="ribbon", fun.data = mean_sdl, fun.ymin = "Lower", fun.ymax = "Upper", alpha=0.3, fill="steelblue")
 
 
@@ -132,7 +188,8 @@ Scobirthpop %>%
   summarise(popsum=sum(Female), birthsum=sum(Total)) %>%
   ungroup() %>%
   filter(agegrp==agecat) %>%
-  mutate(agecat=factor(agecat, labels = c("Under 16", "Under 18", "Under 20"))) %>%
+  mutate(agecat=factor(agecat,
+                       labels = c("Under 16", "Under 18", "Under 20"))) %>%
   mutate(rate=birthsum/popsum) %>%
   ggplot(aes(x=Year, y=rate, col=agecat, group=agecat))+
   geom_line()+
