e873a9b1d047b238e1668b596afa193ce67c5598
Andrew Baxter <andrewbaxter439@gmail.com>
2019-03-14 22:37:35 +0000

    Updating code according to controlled ITS analysis

--- UK countries ITS analysis.R
+++ UK countries ITS analysis.R
@@ -1,8 +1,21 @@
 library(tidyverse)
 library(readxl)
 library(broom)
+library(nlme)
+library(car)
 
-
+testAutocorr <- function(data, model) {
+  print(dwt(model, max.lag=10, alternative="two.sided"))
+  par(cex=0.7, mai=c(0.1,0.1,0.2,0.1))
+  par(fig=c(0.03,1,0.8,1))
+  plot(data$Time[1:25], residuals(model)[1:25])
+  
+  par(fig=c(0.03,0.5,0.05,0.75), new=TRUE)
+  acf(residuals(model))
+  
+  par(fig=c(0.55,1,0.05,0.75), new=TRUE)
+  acf(residuals(model),type='partial')
+}
 
 # Import Scotland data ---------------------------------------------------------------------------------------
 
@@ -21,13 +34,18 @@ Scot.births <- read_xlsx("Downloaded data files/Scotland births.xlsx", range = "
   select(Age = '..33', 1:32) %>% 
   mutate_all(as.numeric)
 
-# Create age calculation groups
+# Create age calculation groups ### NOW DEFUNCT
 
-agegrps<-tibble(Age=factor(c(12:15, 12:17, 12:19)),
-                agegrp=c(1,1,1,1,2,2,2,2,2,2,3,3,3,3,3,3,3,3)) # denominators for rate calc
-agecats<-tibble(Age=factor(c(13:15, 15:17, 15:19)),
-                agecat=c(1,1,1,2,2,2,3,3,3,3,3)) # numerators for rate calc
-agecalcs<-merge(agegrps, agecats)
+birthAgeGrps <- tibble(Age=factor(c(12:15, 12:17, 12:19)),
+                       agegrp=factor(c(1,1,1,1,2,2,2,2,2,2,3,3,3,3,3,3,3,3),
+                                     labels=c("Under 16", "Under 18", "Under 20")))  # Three ranges of births
+
+sumScotBirths <- Scot.births %>% 
+  gather("Year", "Total", -1) %>% 
+  mutate(Year = as.numeric(Year))%>% 
+  merge(birthAgeGrps) %>% 
+  group_by(Year, agegrp) %>% 
+  summarise(sumBirths = sum(Total)) # create summary
 
 # merge with scotland births and population
 
@@ -67,5 +85,122 @@ all.UK.rates %>% filter(Country=="Scotland"|
   geom_smooth(method="lm")}
   
 
+# EngMod - England data in ITS with break at 1999 ------------------------------------------------------------
+
+EngData <- all.UK.rates %>% filter(Country=="England") %>% 
+  select(Country, '1992':'2016') %>% 
+  gather("Year", "Value", -1) %>% 
+  mutate(Cat1 = ifelse(Year<1999, 0, 1),
+         Cat2 = ifelse(Year<=2007, 0, 1),
+         Year=as.numeric(Year),
+         Time = 1:nrow(.),
+         Trend1 = ifelse(Cat1==0, 0, 1:nrow(.)-nrow(filter(., Cat1==0))),
+         Trend2 = ifelse(Cat2==0, 0, 1:nrow(.)-nrow(filter(., Cat2==0))))
+
+EngData %>% ggplot(aes(x=Year, y=Value, group=Cat1)) +
+  geom_point() +
+  geom_smooth(method="lm", se=FALSE)
+
+EngMod <- lm(Value ~ Time + Cat1 + Trend1, data = EngData)  
+summary(EngMod)
+
+# Test for autocorrelation
+
+plot(EngData$Time, residuals(EngMod))
+dwt(EngMod, max.lag=10, alternative="two.sided")
+
+par(mfrow=c(1,2))
+
+# Produce plots
+acf(residuals(EngMod))
+acf(residuals(EngMod),type='partial')
+
+
+# EngMod2 - proposing new effect at Y=2007 -------------------------------------------------------------------
+
+EngData %>% ggplot(aes(x=Year, y=Value, group=interaction(Cat1, Cat2))) +
+  geom_point() +
+  geom_smooth(method="lm")
+
+EngMod2 <- lm(Value ~ Time + Cat1 + Trend1 + Cat2 + Trend2, data = EngData)  
+summary(EngMod2)
+
+# Test for autocorrelation
+
+plot(EngData$Time, residuals(EngMod2))
+dwt(EngMod2, max.lag=10, alternative="two.sided")
+
+par(mfrow=c(1,2))
+
+# Produce plots
+acf(residuals(EngMod2))
+acf(residuals(EngMod2),type='partial')
+
+EngMod2_p3 <- gls(Value ~ Time + Cat1 + Trend1 + Cat2 + Trend2,
+                  data=EngData,
+                  correlation = corARMA(p=3, form=~Time),
+                  method = "ML")
+summary(EngMod2_p3)
+
+EngMod2_p3 %>% update(correlation=corARMA(p=4, form=~Time)) %>% 
+  anova(EngMod2_p3)
+
+EngMod2_p3 %>% update(correlation=corARMA(p=3, q=1, form=~Time)) %>% 
+  anova(EngMod2_p3)
+
+
+# Preliminary analysis - Eng v Scot --------------------------------------------------------------------------
+## Setting up data
+ EngScotContro <-
+  all.UK.rates %>% filter(Country=="Scotland"|
+                          Country=="England") %>% 
+  gather("Year", "Value", -1) %>%
+  filter(Year>1991) %>%
+  arrange(Country) %>%
+  mutate(England = ifelse(Country=="England", 1, 0),
+         Year=as.numeric(Year),
+         Time=Year-min(Year)+1,
+         Cat1 = ifelse(Year<1999, 0, 1),
+         Cat2 = ifelse(Year<=2007, 0, 1),
+         Trend1 = ifelse(Cat1==0, 0, Year-1998),
+         Trend2 = ifelse(Cat2==0, 0, Year-2007)) %>% 
+  mutate_at(., colnames(.)[5:9], list(Eng = ~.*England))  # Potentially not necessary
+
+## Comparing for pre-post 1999 --------------------------------------------
+
+EngScotContro %T>% 
+  {print(ggplot(., aes(Year, Value, group=interaction(Country, Cat1), col=Country))+
+  geom_point() + geom_smooth(method="lm", se=FALSE))} %>% 
+  lm(Value ~ Time + England + Time*England + Cat1 + Trend1 + Cat1*England + Trend1*England, data=.) %>% 
+  assign("modScot99",., envir = .GlobalEnv) %>% 
+  print(.$coefs)
+#  summary()
+
+testAutocorr(EngScotContro, modScot99)
   
-  
+## Comparing for pre-post 2007
+
+EngScotContro %T>% 
+{print(ggplot(., aes(Year, Value, group=interaction(Country, Cat2), col=Country))+
+         geom_point() + geom_smooth(method="lm"))} %>% 
+  lm(Value ~ Time + England + Time*England + Cat2 + Trend2 + Cat2*England + Trend2*England, data=.) %>% 
+  summary()
+
+## Comparing for thee stages, split at 1999 and 2007
+
+EngScotContro %T>% 
+{print(ggplot(., aes(Year, Value, group=interaction(Country, Cat1, Cat2), col=Country))+
+         geom_point() + geom_smooth(method="lm"))} %>% 
+  lm(Value ~ Time + 
+       England + 
+       Time*England + 
+       Cat1 + 
+       Trend1 + 
+       Cat1*England + 
+       Trend1*England +
+       Cat2 +
+       Trend2 +
+       Cat2*England +
+       Trend2*England,
+     data=.) %>% 
+  summary()
--- graphs of teen births.R
+++ graphs of teen births.R
@@ -3,11 +3,13 @@ library(rgeos)
 library(rnaturalearth)
 library(rnaturalearthdata)
 library(tidyverse)
-library(ggplot2)
+library(sp)
+
+
+# Mapping Europe (code from https://bhaskarvk.github.io/) ----------------------------------------------------
 
 world <- rnaturalearthdata::countries50
 europe <- world[world$region_un=="Europe"&world$name!='Russia',]
-# plot(europe)
 
 # Let's add a unique ID column to our data.
 {{europe@data$id <- row.names(europe@data)}}
@@ -22,19 +24,22 @@ europe.clipped <-
 {{  rgeos::gIntersection(europe, europe.bbox, byid = TRUE, id=europe$id)}}
 
 # tidy up the data for ggplot2
-europe.tidy <- broom::tidy(europe.clipped)
-europe.tidy <- dplyr::left_join(europe.tidy, europe@data %>% select(name, id), by='id')
+europe.tidy <- tidy(europe.clipped)
+europe.tidy <- left_join(europe.tidy, europe@data %>% select(name, id), by='id')
 
-# merge with births data
 
-EU_nat2 <- europe.tidy %>%
-  mutate(sovereignt=ifelse(sovereignt=="Czech Republic", "Czechia", sovereignt)) %>%
-  rename(Country=sovereignt) %>%
+# Merging map with births data -------------------------------------------------------------------------------
+
+EU_nat <- europe.tidy %>%
+  mutate(name=ifelse(name=="Czech Republic", "Czechia", name)) %>%
+  rename(Country=name) %>%
   select(long, lat, order, group, Country)
 
-birthsandborders <- merge(birthrates[c(1,2,4,7)], EU_nat2, by="Country")
+birthsandborders <- merge(birthRates[c(2,3,4,7)], EU_nat, by="Country")
 
-birthsandborders %>%
+# Animated plots ----------------------------------------------------------------------------------------------
+
+birthsandborders %>%  # Attempted animation 1 - facets
   arrange(Country, order) %>%
   ggplot(aes(x=long, y=lat,group=group, fill=rate)) +
   geom_polygon(data=EU_nat, aes(x=long, y=lat, group=group), fill="grey") +
@@ -46,7 +51,7 @@ birthsandborders %>%
         plot.title=element_text(size=20, hjust=0.5),
         plot.margin=grid::unit(c(0,0,0,0), "mm"))    +
   coord_map(projection = "ortho", orientation=c(35, 10, 0), xlim = c(-30, 35), ylim=c(32, 72)) +
-  facet_wrap(~agecat) +
+  facet_wrap(~agegrp) +
   #    ylim(32, NA) +
   #    xlim(-30, 35)+
   scale_fill_continuous(name="Rates of pregnancy\n
@@ -55,24 +60,25 @@ birthsandborders %>%
   transition_states(Year, transition_length = 1, state_length = 2) +
   labs(title='{previous_state}')
 
-u18anim <- birthsandborders %>%
+
+u18anim <- birthsandborders %>%  # Animation for under-18 rate
   arrange(Country, order) %>%
-  filter(agecat=="Under 18", Year<2014, Country!="Bulgaria") %>%
-  mutate(rate=1000*rate) %>%
+  filter(agegrp=="Under 18", Year<2014, Country!="Bulgaria") %>%  # Bulgaria's abnormally high rate
   ggplot(aes(x=long, y=lat,group=group, fill=rate)) +
-  geom_polygon(data=EU_nat2, aes(x=long, y=lat, group=group), fill="grey") +
+  geom_polygon(data=EU_nat, aes(x=long, y=lat, group=group), fill="grey") +  # background countries with no data
   geom_polygon() + 
   theme(line=element_blank(),
         rect=element_blank(),
         axis.title=element_blank(),
         axis.text=element_blank(),
-        plot.title=element_text(size=20, face="bold", family="arial", hjust=0),
-        plot.subtitle=element_text(size=15, family="arial"),
-        plot.margin=margin(1, 0, 0, 0, "cm"))    +
+        plot.title=element_text(size=20, face="bold", family="arial", hjust=0, margin = margin(b=10, l=150)),
+        plot.subtitle=element_text(size=15, family="arial", margin = margin(b=-60, l=150)),
+        plot.margin=margin(1, 0,-3,0, "cm"))    +
   coord_map(projection = "ortho", orientation=c(35, 10, 0), xlim = c(-10, 30), ylim=c(29, 75)) +
-#  scale_fill_continuous(name="Rates of births\nto under-18s\n(per 1000 women)") +
   scale_fill_continuous(name="Rates of births\nto under-18s\n(per 1000 women)", type="viridis", option="A") +
   transition_states(Year, transition_length = 1, state_length = 1, wrap=FALSE) +
   labs(title = "Human Fertility Database - teenage birth rates", subtitle="Year: {closest_state}")
 
-animate(u18anim, duration=20, fps=20, start_pause = 20, end_pause = 40,renderer=ffmpeg_renderer())
+animate(u18anim, duration=15, fps=20, height = 600, width=700, start_pause = 10, end_pause = 40) %>% 
+  anim_save("teenagebirthrates.gif", animation = (.), path = getwd())# Unit comp - no ffmpeg
+# animate(u18anim, duration=20, fps=20, start_pause = 20, end_pause = 40,renderer=ffmpeg_renderer())
