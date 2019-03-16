a211d40bc7b504ee591e9055800d40799f643158
Andrew Baxter <andrewbaxter439@gmail.com>
2019-03-16 00:29:15 +0000

    Completed ITS analysis for Scot controls
    * Two-intervention model - 1999 and 2007, graphed with CIs
    
    * Phase-in model, beginning 2007, graphed with CIs

--- UK countries ITS analysis.R
+++ UK countries ITS analysis.R
@@ -4,17 +4,31 @@ library(broom)
 library(nlme)
 library(car)
 
-testAutocorr <- function(data, model) {
-  print(dwt(model, max.lag=10, alternative="two.sided"))
+testAutocorr <- function(data, model, max.lag=10) {
+  print(dwt(model, max.lag=max.lag, alternative="two.sided"))
   par(cex=0.7, mai=c(0.1,0.1,0.2,0.1))
   par(fig=c(0.03,1,0.8,1))
-  plot(data$Time[1:25], residuals(model)[1:25])
+  plot(data$Time[1:25], residuals(model)[1:25],
+       type='o',
+       pch=16,
+       col="red")
   
   par(fig=c(0.03,0.5,0.05,0.75), new=TRUE)
   acf(residuals(model))
   
   par(fig=c(0.55,1,0.05,0.75), new=TRUE)
   acf(residuals(model),type='partial')
+}
+
+constructCIRibbon <- function(newdata, model) {
+  newdata <- newdata %>% 
+    mutate(Value = predict(model, newdata=newdata))
+    mm <- model.matrix(as.formula(paste0("~ ", model$call$model[3])),
+                 data=newdata)
+    vars <- mm %*% vcov(model) %*% t(mm)
+    sds <- sqrt(diag(vars))
+    newdata <- newdata %>% mutate(lowCI = Value - 1.96 * sds,
+      HiCI = Value + 1.96 * sds)
 }
 
 # Import Scotland data ---------------------------------------------------------------------------------------
@@ -66,7 +80,7 @@ Scot.birth.rates <- Scot.births %>%
 
 
 
-# Simple reading of conception rates from combined data ------------------------------------------------------
+# all.UK.rates - Import conception rates for GBR countries -------------------------------------------
 
 all.UK.rates <- read_xlsx("Conception rates by age and country.xlsx", sheet = "Under 18")
 
@@ -148,6 +162,9 @@ coef(EngMod2_q3)
 EngMod2_q3 %>% update(correlation=corARMA(q=4, form=~Time)) %>% 
   anova(EngMod2_q3)
 
+EngMod2_q3 %>% update(correlation=NULL) %>% 
+  anova(EngMod2_q3)
+
 EngMod2_q3 %>% update(correlation=corARMA(q=3, p=1, form=~Time)) %>% 
   anova(EngMod2_q3)  # Significantly different
 
@@ -174,8 +191,8 @@ EngData %>%
   geom_vline(xintercept=7.5, linetype="dotted", col="#000000CC") +
   geom_vline(xintercept=16.5, linetype="dotted", col="#000000CC")
 
-# Preliminary analysis - Eng v Scot --------------------------------------------------------------------------
-## Setting up data
+# EngScotContro data setup - Eng v Scot --------------------------------------------------------------------------
+
  EngScotContro <-
   all.UK.rates %>% filter(Country=="Scotland"|
                           Country=="England") %>% 
@@ -189,9 +206,9 @@ EngData %>%
          Cat2 = ifelse(Year<=2007, 0, 1),
          Trend1 = ifelse(Cat1==0, 0, Year-1998),
          Trend2 = ifelse(Cat2==0, 0, Year-2007)) %>% 
-  mutate_at(., colnames(.)[5:9], list(Eng = ~.*England))  # Potentially not necessary
+  mutate_at(., colnames(.)[5:9], list(Eng = ~._Eng))  # Potentially not necessary
 
-## Comparing for pre-post 1999 --------------------------------------------
+## modScot99 - Comparing for pre-post 1999 --------------------------------------------
 
 EngScotContro %T>% 
   {print(ggplot(., aes(Year, Value, group=interaction(Country, Cat1), col=Country))+
@@ -201,7 +218,7 @@ EngScotContro %T>%
   print(.$coefs)
 #  summary()
 
-testAutocorr(EngScotContro, modScot99)  # Indicates AR3
+testAutocorr(EngScotContro, modScot99)  # Decay in ACF, sig at lag=3 in PACF. Indicates AR3
 
 modScot99_p3 <- gls(Value ~ Time + England + Time_Eng + Cat1 + Trend1 + Cat1_Eng + Trend1_Eng,
                      data=EngScotContro,
@@ -212,10 +229,13 @@ coef(modScot99_p3)
 
 ## Testing other models
 modScot99_p3 %>% update(correlation = corARMA(p=4, form = ~Time|England)) %>% 
-  anova(modScot99)
+  anova(modScot99_p3)
+
+modScot99_p3 %>% update(correlation = NULL) %>% 
+  anova(modScot99_p3)
 
 modScot99_p3 %>% update(correlation = corARMA(p=3, q=1, form = ~Time|England)) %>% 
-  anova(modScot99)  # Conclusion: p3 is a bad model!
+  anova(modScot99_p3)  # Conclusion: p3 is a bad model!
 
 modScot99_p3_cfac <- tibble(Time=c(8:25),
                              Trend1=c(1:18),
@@ -235,29 +255,167 @@ EngScotContro %>%
   scale_x_continuous(breaks=c(4,9,14,19,24),labels = seq(1995, 2015, by=5)) +
   geom_vline(xintercept=7.5, linetype="dotted", col="#000000CC")
     
-## Comparing for pre-post 2007
+## modScot07 - Comparing for pre-post 2007 --------------
 
 EngScotContro %T>% 
 {print(ggplot(., aes(Year, Value, group=interaction(Country, Cat2), col=Country))+
          geom_point() + geom_smooth(method="lm"))} %>% 
-  lm(Value ~ Time + England + Time*England + Cat2 + Trend2 + Cat2*England + Trend2*England, data=.) %>% 
+  lm(Value ~ Time + England + Time_Eng + Cat2 + Trend2 + Cat2_Eng + Trend2_Eng, data=.) %>% 
+  assign("modScot07",., envir = .GlobalEnv) %>% 
   summary()
 
-## Comparing for thee stages, split at 1999 and 2007 --------------------
+## modScot99_07 - Comparing for thee stages, split at 1999 and 2007 --------------------
 
 EngScotContro %T>% 
 {print(ggplot(., aes(Year, Value, group=interaction(Country, Cat1, Cat2), col=Country))+
          geom_point() + geom_smooth(method="lm", se=FALSE))} %>% 
   lm(Value ~ Time + 
        England + 
-       Time*England + 
+       Time_Eng + 
        Cat1 + 
        Trend1 + 
-       Cat1*England + 
-       Trend1*England +
+       Cat1_Eng + 
+       Trend1_Eng +
        Cat2 +
        Trend2 +
-       Cat2*England +
-       Trend2*England,
+       Cat2_Eng +
+       Trend2_Eng,
      data=.) %>% 
+  assign("modScot99_07",., envir = .GlobalEnv) %>% 
   summary()
+
+## test autocorrelation
+testAutocorr(EngScotContro, modScot99_07)  # Assume NULL for now
+
+modScot99_07_null <- gls(Value ~ Time + 
+                           England + 
+                           Time_Eng + 
+                           Cat1 + 
+                           Trend1 + 
+                           Cat1_Eng + 
+                           Trend1_Eng +
+                           Cat2 +
+                           Trend2 +
+                           Cat2_Eng +
+                           Trend2_Eng,
+                         data=EngScotContro,
+                         correlation = NULL,
+                         method="ML")
+
+summary(modScot99_07_null)
+
+modScot99_07_null_cfac <- EngScotContro[8:25,] %>% 
+  select(Time, England, Cat1, Cat2, Trend1, Trend2, Time_Eng) %>% 
+  mutate(Cat1_Eng = c(rep(0,9), rep(1,9)),
+         Trend1_Eng = c(rep(0,9), 10:18),
+         Cat2_Eng = 0,
+         Trend2_Eng = 0)
+
+modScot99_07_null_cfac <- 
+  modScot99_07_null_cfac %>% 
+  left_join(constructCIRibbon(modScot99_07_null_cfac, modScot99_07_null))
+
+EngScotContro %>%
+  mutate(predict = predict(modScot99_07_null)) %>%
+  ggplot(aes(Time, Value, col = Country, group = interaction(Country, Cat1, Cat2))) +
+  geom_point() +
+  geom_smooth(  # predicted values and 95% Confidence Interval
+    data = modScot99_07_null_cfac,
+    aes(
+      x = Time,
+      y = Value,
+      ymin = lowCI,
+      ymax = HiCI,
+      group = Cat2
+    ),
+    stat = 'identity',
+    linetype = "dashed",
+    col = "#FC8D62",
+    fill = "#FC8D62",
+    size = 1,
+    inherit.aes = FALSE
+  ) +
+  geom_line(aes(y = predict), size = 1) +
+  scale_x_continuous(breaks = c(4, 9, 14, 19, 24),
+                     labels = seq(1995, 2015, by = 5)) +
+  geom_vline(xintercept = 7.5,
+             linetype = "dotted",
+             col = "#000000CC") +
+  geom_vline(xintercept = 16.5,
+             linetype = "dotted",
+             col = "#000000CC") +
+  theme(panel.background = element_blank()) +
+  ylab("Rate of pregnancies to under-18s, per 1,000") +
+  xlab("Year")
+
+# modScotPI - Comparing pre-1999 and post-2007, proposing phase-in -------------------------------------------
+
+EngScotContro %>% 
+  filter(Year<1999|Year>2007) %T>% 
+{print(ggplot(., aes(Year, Value, group=interaction(Country, Cat1, Cat2), col=Country))+
+         geom_point() + geom_smooth(method="lm", se=FALSE))} %>% 
+  lm(Value ~ Time + England + Time_Eng + Cat2 + Trend2 + Cat2_Eng + Trend2_Eng, data=.) %>% 
+  assign("modScotPI",., envir = .GlobalEnv) %>% 
+  summary()
+
+testAutocorr(EngScotContro, modScotPI, 14)  # Plotting residuals (top) invalid - data not filtered
+                                            # Possibly no autocorrelation? Sig at PACF 12 and 13
+
+modScotPI_null <- gls(Value ~ Time + England + Time_Eng + Cat2 + Trend2 + Cat2_Eng + Trend2_Eng,
+                       data=EngScotContro %>% filter(Year<1999|Year>2007),
+                      correlation = NULL,
+                      method="ML")
+
+summary(modScotPI_null)
+confint(modScotPI_null)
+
+modScotPI_null_cfac <- tibble(Time=c(17:25),
+                            England=1,
+                            Cat2=1,
+                            Trend2=c(1:9),
+                            Time_Eng=17:25,
+                            Cat2_Eng=0,
+                            Trend2_Eng=0)  # Emlulating England with no added effect
+
+modScotPI_null_cfac <- 
+  modScotPI_null_cfac %>% 
+  left_join(constructCIRibbon(modScotPI_null_cfac, modScotPI_null))
+
+EngScotContro %>%
+  filter(Year < 1999 | Year > 2007) %>%
+  mutate(predict = predict(modScotPI_null)) %>%
+  ggplot(aes(Time, Value, col = Country, group = interaction(Country, Cat2))) +
+  geom_point(data = EngScotContro) +
+  geom_smooth(  # predicted values and 95% Confidence Interval
+    data = modScotPI_null_cfac,
+    aes(
+      x = Time,
+      y = Value,
+      ymin = lowCI,
+      ymax = HiCI
+    ),
+    stat = 'identity',
+    linetype = "dashed",
+    col = "#FC8D62",
+    fill = "#FC8D62",
+    size = 1,
+    inherit.aes = FALSE
+  ) +
+  geom_line(aes(y = predict), size = 1) +
+  scale_x_continuous(breaks = c(4, 9, 14, 19, 24),
+                     labels = seq(1995, 2015, by = 5)) +
+  geom_vline(xintercept = 7.5,
+             linetype = "dotted",
+             col = "#000000CC") +
+  theme(panel.background = element_blank()) +
+  geom_rect(
+    xmin = 7.5,
+    xmax = 16.5,
+    ymin = 0,
+    ymax = 60,
+    fill = "grey",
+    alpha = 0.01,
+    inherit.aes = FALSE
+  ) +
+  ylab("Rate of pregnancies to under-18s, per 1,000") +
+  xlab("Year")
