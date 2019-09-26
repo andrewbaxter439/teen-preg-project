8a21e315a184ea795257732a3ddb4871e776292a
Andrew Baxter <andrewbaxter439@gmail.com>
2019-09-26 11:25:49 +0100

    clustering tidy

--- R/clustering countries.R
+++ R/clustering countries.R
@@ -1,16 +1,26 @@
 library(dplyr)
 library(ggplot2)
 library(SPHSUgraphs)
-source("R/Synth_data.R")
+load("Data/synth_data.rdata")
+library(ggrepel)
 
-df <- synthData %>% 
-  filter(Year == 1998, agegrp == "Under 18", !is.na(GDPperCap)) %>% 
+# Under-18 birth rates ---------------------------------------------------------------------------------------
+
+
+# ** dataprep ------------------------------------------------------------------------------------------------
+
+df <- synthData_u18 %>% 
+  filter(Year == 1998,!is.na(GDPperCap)) %>% 
   select(Country, GDPperCap, rate)
 
-df %>% 
-  mutate(cluster = kmeans(scale(df[,2:3]), 4)$cluster) %>% 
-  ggplot(aes(GDPperCap, rate, col = factor(cluster))) +
-  geom_text(aes(label = Country))
+
+matrix_df <- scale(as.matrix(df[,2:3]))
+row.names(matrix_df) <- df$Country
+
+
+# ** k-means clustering --------------------------------------------------------------------------------------
+
+
 
 kmeans_table <- c()
 for (i in 1:15){
@@ -22,14 +32,28 @@ tibble(kmeans_table, k = 1:15) %>%
   geom_point() +
   geom_line()
 
-matrix_df <- scale(as.matrix(df[,2:3]))
-row.names(matrix_df) <- df$Country
+
+
+df %>% 
+  mutate(cluster = kmeans(scale(df[,2:3]), 4)$cluster) %>% 
+  ggplot(aes(GDPperCap, rate, col = factor(cluster))) +
+  geom_text_repel(aes(label = Country), size = 8) +
+  theme_sphsu_light() +
+  theme(panel.grid = element_blank(),
+        panel.background = element_blank(),
+        legend.position = "none")
+
+# ** Hierarchical clustering ---------------------------------------------------------------------------------
+
 
 hclust.out <- hclust(dist(matrix_df))
 plot(hclust.out)
 
+# Under 20 pregnancy rates  ----------------------------------------------------------------------------------
 
-# Under 18 ---------------------------------------------------------------------------------------------------
+
+# ** k-means clustering ---------------------------------------------------------------------------------
+
 
 df1 <- synthData_u20 %>% 
   filter(Year == 1998, Country != "New Zealand") %>%
@@ -51,14 +75,39 @@ tibble(clusters = 1:15, wss = mt1_ks) %>%
   geom_point() +
   geom_line()
 
-hclust.out1 <- hclust(dist(mt1))
 
 
 df1 %>% 
   mutate(cluster1 = factor(kmeans(scale(as.matrix(df1[,2:3])), 4)$cluster),
          cluster2 = factor(cutree(hclust.out1, k = 4))) %>% 
   ggplot(aes(GDPperCap, pRate, col = cluster2, label = Country)) +
-  geom_text()
+  geom_text_repel(aes(label = Country), size = 8) +
+  theme_sphsu_light() +
+  theme(panel.grid = element_blank(),
+        panel.background = element_blank(),
+        legend.position = "none")
 
 
+# ** Hierarchical clustering ---------------------------------------------------------------------------------
+
+
+hclust.out1 <- hclust(dist(mt1))
 plot(hclust.out1)
+
+
+
+# Clustering by all predictors -------------------------------------------------------------------------------
+
+
+# Under-18 ---------------------------------------------------------------------------------------------------
+
+synthData_u18 %>% 
+  filter(Year<1999) %>% 
+  mutate(period = ifelse(Year<1988, 1,
+                                ifelse(Year<1990, 2,
+                                       ifelse(Year<1993, 3, 4)))) %>% 
+  group_by(Country) %>% 
+  mutate_at(vars(5:8), mean) %>% 
+  group_by(Country, period) %>% 
+  summarise_all(mean) %>% 
+  filter_all(!anyNA(.))
